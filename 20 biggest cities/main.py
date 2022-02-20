0e90413ed4ef66e877a07be6e4d584d5025da3d9
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-02-20 16:07:14 +0000

    Update main.py

--- 20 biggest cities/main.py
+++ 20 biggest cities/main.py
@@ -1,1 +1,14 @@
-test
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+url = "https://www.archdaily.com/906605/the-20-largest-cities-in-the-world-of-2018"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+cities = soup.find_all(name='h3')
+
+for city in cities:
+    print(city)
+
