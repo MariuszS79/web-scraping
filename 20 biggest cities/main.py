529b42c78e6daa26ff441e26e34af7d4c09f684f
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-02-20 18:39:24 +0000

    1

--- 20 biggest cities/main.py
+++ 20 biggest cities/main.py
@@ -1,5 +1,4 @@
 import requests
-import lxml
 from bs4 import BeautifulSoup
 
 url = "https://www.archdaily.com/906605/the-20-largest-cities-in-the-world-of-2018"
@@ -7,8 +6,50 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-cities = soup.find_all(name='h3')
+cities = soup.find_all("h3")
 
-for city in cities:
-    print(city)
+city_list_raw = [city.getText() for city in cities]
+city_list_raw = city_list_raw[0:20]
+
+
+position = []
+city = []
+country = []
+population = []
+
+for i in range(len(city_list_raw)):
+    splitted = city_list_raw[i].split()
+    position.append(splitted[0])
+    position[i] = position[i].replace("-","")
+    city.append(splitted[1])
+    city[i] = city[i].replace(",", "")
+    country.append(splitted[2])
+
+pop = soup.find_all(lambda tag:tag.name=="p" and "Population:" in tag.text)
+pop_list_raw = [p.getText() for p in pop]
+pop_list_raw = pop_list_raw[::2]
+
+
+for i in range(len(pop_list_raw)):
+    splitted = pop_list_raw[i][12:]
+    population.append(splitted)
+    population[i] = population[i].replace(" ", "")
+    population[i] = population[i].replace(",", "")
+    population[i] = int(population[i])
+
+zipped = zip(position, city, country, population)
+
+for a, b, c, d in zipped:
+    print(a, b, c, d)
+
+
+
+
+
+
+
+
+
+
+
 
