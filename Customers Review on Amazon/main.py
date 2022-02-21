713c64700731bf0d069fca323479b27701d2ca82
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-02-21 16:19:24 +0000

    Update main.py

--- Customers Review on Amazon/main.py
+++ Customers Review on Amazon/main.py
@@ -1,3 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import lxml
+
+# link to Amazon Fire HD 8 Tablet on amazon.co.uk
+url = "https://tinyurl.com/3tcbjch4"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/98.0.4758.82 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+
+total_ratings = soup.find(id="acrCustomerReviewText").get_text()
+total_ratings = total_ratings.split("ratings")
+total_ratings = int(total_ratings[0].replace(",", ""))
+
+
+
+
+
+
+print(five_stars)
+
