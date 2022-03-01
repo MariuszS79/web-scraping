bb0fa5a6d7f2231d1842e2e73e23d36b24abfc55
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-03-01 13:06:07 +0000

    Update main.py

--- Customers Review on Amazon/main.py
+++ Customers Review on Amazon/main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import lxml
+from lxml import etree
 
 # link to Amazon Fire HD 8 Tablet on amazon.co.uk
 url = "https://tinyurl.com/3tcbjch4"
@@ -13,9 +14,18 @@ header = {
 response = requests.get(url, headers=header)
 
 soup = BeautifulSoup(response.content, "lxml")
+dom = etree.HTML(str(soup))
 
 total_ratings = soup.find(id="acrCustomerReviewText").get_text()
 total_ratings = total_ratings.split("ratings")
 total_ratings = int(total_ratings[0].replace(",", ""))
+
+print(total_ratings)
+
+average_rating = soup.find(class_="a-icon-alt").get_text()
+print(average_rating)
+
+five_stars = soup.find(class_="a-link-normal 5star").get_text()
+print(five_stars)
 
 
