b03736054b64ca47a2827a9351e5bf28dca317ed
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-03-02 14:21:51 +0000

    Update main.py

--- 10 biggest countries/main.py
+++ 10 biggest countries/main.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from lxml import html
 
-url = "https://worldpopulationreview.com/country-rankings/largest-countries-in-the-world"
+url = "https://www.worlddata.info/the-largest-countries.php"
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
 
-countries = soup.find_all("tr")
+# tree = html.fromstring(response.content)
+# number_one = tree.xpath('//*[@id="example2"]/tbody/tr[1]/td[2]')
+# print(number_one)
 
-print(countries)
+test = soup.find(class_="std100 hover")
+print(test.text)
+lista = [i.getText() for i in test]
+print(lista)
+
+
+
